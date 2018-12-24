bd4e9b0bf7f7eb11886d01565309ee2c07624d7e
jrochaa <1161838@isep.ipp.pt>
2018-12-24 15:24:28 +0000

    Drawer nav progress

--- Mindera/app/src/main/java/com/example/jpfr8/mindera/MainActivity.java
+++ Mindera/app/src/main/java/com/example/jpfr8/mindera/MainActivity.java
@@ -19,6 +19,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
